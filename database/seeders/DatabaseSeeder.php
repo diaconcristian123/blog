28b277bcd329f51a8abf9696711ceb146b4cf711
Furtuna Eugeniu <furtuna.eugeniu@elev.cihcahul.md>
2024-02-02 09:36:03 +0200

    Am instalat Spatiu,Breeze

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,19 +4,41 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use PhpParser\Node\Stmt\Foreach_;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
+    private $permissions =[
+        'role-list',
+        'role-create',
+        'role-edit',
+        'role-delete'
+
+    ];
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+       foreach($this->permissions as $permission){
+        Permission::create(['name'=>$permission]);
+       }
+       $user = User::create([
+        'name'=>'Diacon Cristian',
+        'email'=>'diacon.cristian@elev.cihcahul.md',
+        'password'=> Hash::make('Cristi2004')
+       ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+       $role =Role::create(['name' => 'Admin']);
+
+       $permissions =Permission::pluck('id','id')->all();
+
+       $role->syncPermissions($permissions);
+
+       $user->assignRole([$role->id]);
     }
 }
